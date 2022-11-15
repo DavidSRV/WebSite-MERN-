9f0abe0d80642e56d0c31d7beec3e29856fe62c3
David Ruiz <david.ruizvargas@hotmail.com>
2022-11-14 23:19:35 -0500

    Feat: Se inicia proyecto

---
+++ src/page/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <section id="home">
+      <div className="container">
+        <div className="row justify-content-center mt-5 ">
+          <div className="col-md-8">
+            <h1 className="display-4 fw-bolder mb-4 text-center">Feels the Fresh Bussinnes Perspective</h1>
+            <p className="lead text-center fs-4 mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis cum ab ad consectetur voluptatibus quidem architecto tenetur ratione, aliquam non, tempore voluptates excepturi harum earum mollitia maxime possimus laudantium ullam.</p>
+            <div className="buttons d-flex justify-content-center">
+            <button className="btn btn-light me-4 rounded-pill px-4 py-2">Get Quote</button>
+            <button className="btn btn-outline-light rounded-pill px-4 py-2">Our Services</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
