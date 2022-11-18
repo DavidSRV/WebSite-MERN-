839a777017ac4ca523d857cb5224b6d8a95c4533
David Ruiz <david.ruizvargas@hotmail.com>
2022-11-17 21:06:21 -0500

    Se termina backend

---
+++ Server/app.js
@@ -0,0 +1,103 @@
+//Import all dependencies
+const dotenv = require('dotenv');
+const express =  require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
+
+const app = express();
+
+
+//Config ENV file and Require connection file 
+
+dotenv.config({path: './config.env'});
+
+require('./db/conn');
+const port =  process.env.PORT;
+
+// Require Model 
+
+const Users = require('./models/userSchema');
+
+//Method for get data and cookies from fronted
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+// Registration
+
+app.post('/register', async (req, res)=>{
+    try {
+        //get body or Data
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const createUser = new Users({
+            username,
+            email,
+            password
+        });
+
+        //Save Method is Used to Create User Or insert user
+        //First hashing before insert user
+        const created = await createUser.save();
+        console.log(created);
+        res.status(200).send("Registered")
+        
+    } catch (error) {
+        res.status(400).send(error)
+
+    }
+})
+
+
+// Login user
+
+app.post('/login', async (req, res)=>{
+
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        //Find user if exist
+
+        const user = await Users.findOne({email});
+        if(user){
+            const isMatch = await bcryptjs.compare(password, user.password);
+
+            if(isMatch){
+                //Generate Token wich is define in user schema
+                const token = await user.generateToken(); 
+                res.cookie("jwt", token, {
+                    // Expire Token in 24h 
+                    expires : new Date(Date.now() + 86400000),
+                    hhtpOnly : true
+                })
+                res.status(200).send("LoggendIn")
+            }else{
+                res.status(400).send("Invalid Credendital")
+            }
+        }else{
+            res.status(400).send("Invalid Credendital")
+        }
+
+    } catch (error) {
+        
+        res.status(400).send(error)
+
+    }
+
+})
+
+
+
+//Run Server
+
+app.listen(port, ()=> {
+    console.log("Server is listening ")
+})
